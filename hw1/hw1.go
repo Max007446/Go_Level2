cda9c8c3e47eaec04b283b414309e43b2f18b1e3
MaxPsk <max00744@gmail.com>
2021-03-03 23:38:39 +0300

    hw2

--- hw1/hw1.go
+++ hw1/hw1.go
@@ -3,17 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
+type ErrorWithTime struct {
+	text string
+	time string
+}
+
+func New(text string) error {
+	t := time.Now()
+	return &ErrorWithTime{
+		text: text,
+		time: t.Format(time.RFC3339),
+	}
+}
+func (e *ErrorWithTime) Error() string {
+	return fmt.Sprintf("error: %s\ntime error:\n%s", e.text, e.time)
+}
+
 func main() {
+
 	var err error
 	defer func() {
 		if v := recover(); v != nil {
-			fmt.Println("Предупреждение:", v)
+			fmt.Println(v)
 		}
 	}()
 	_, err = os.Open("max.go")
 	if err != nil {
+		err = New("my error")
 		panic(err)
 	}
 
